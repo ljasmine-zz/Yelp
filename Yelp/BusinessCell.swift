675f35d6f20ec2ef422b1311cdc0437236676f84
Jasmine Lee <jasmine.lee@airbnb.com>
2016-10-25 20:23:20 -0700

    Updated tableView to use auto layout

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -38,7 +38,15 @@ class BusinessCell: UITableViewCell {
 
         thumbImageView.layer.cornerRadius = 5
         thumbImageView.clipsToBounds = true
+
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
         // Initialization code
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
